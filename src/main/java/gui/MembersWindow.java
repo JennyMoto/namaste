3b01da6daf790e68f2af11888d0d65f9d696ef29
Jenny <kaminskak@gmail.com>
2018-01-30 17:43:15 +0100

    Added MembersDao, changed MembersViewModel, MembersViewTable, MembersView

--- src/main/java/gui/MembersWindow.java
+++ src/main/java/gui/MembersWindow.java
@@ -1,6 +1,10 @@
 package gui;
 
 
+import dao.MembersDao;
+import gui.membersView.MembersViewTable;
+import org.hibernate.Session;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -9,15 +13,22 @@ import java.awt.event.ActionListener;
 /**
  * Created by Jenny on 27.01.2018.
  */
-public class MembersWindow extends JPanel implements ActionListener{
+public class MembersWindow extends JPanel implements ActionListener {
+
+    private MembersViewTable table;
+
 
     JButton addMember = new JButton("Add Member");
     JButton remMember = new JButton("Remove Member");
-    JButton back = new JButton("Back");
-    JTextArea searchMember = new JTextArea("Wpisz imię i nazwisko");
-
+    JTextArea searchMember = new JTextArea("Wyszukaj...");
 
     public MembersWindow() {
+
+        Session memberSess = HibernateUtil.openSession();
+        MembersDao memberD = new MembersDao(memberSess);
+        table = new MembersViewTable(memberD.findAll());
+        JScrollPane sp = new JScrollPane(table);
+        memberSess.close();
 
 //        this.setLocationRelativeTo(null);
 //        this.setModal(true);
@@ -26,7 +37,6 @@ public class MembersWindow extends JPanel implements ActionListener{
 
         addMember.addActionListener(this);
         remMember.addActionListener(this);
-        back.addActionListener(this);
 
         addMember.setBackground(Color.lightGray);
         addMember.setFont(new Font("Arial", Font.BOLD, 18));
@@ -36,16 +46,15 @@ public class MembersWindow extends JPanel implements ActionListener{
         remMember.setFont(new Font("Arial", Font.BOLD, 18));
         remMember.setForeground(Color.darkGray);
 
-        back.setBackground(Color.lightGray);
-        back.setFont(new Font("Arial", Font.BOLD, 18));
-        back.setForeground(Color.darkGray);
-
         searchMember.setFont(new Font("Arial", Font.BOLD, 18));
         searchMember.setForeground(Color.darkGray);
 
-        add(addMember);
-        add(remMember);
-        add(back);
+        JPanel panel = new JPanel();
+        panel.add(addMember);
+        panel.add(remMember);
+        panel.setPreferredSize(new Dimension(500, 50));
+        add(sp);
+        add(panel);
         add(searchMember);
 
         this.setVisible(true);
@@ -55,17 +64,133 @@ public class MembersWindow extends JPanel implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
 
-        if (source == addMember){
-            System.out.println("Dodaje członka");
+        if (source == addMember) {
+            new DataSaver();
+        } else if (source == remMember) {
+            Members member = table.getModel().getList(table.getSelectedRow());
+
+            Session ses = HibernateUtil.openSession();
+                org.hibernate.Transaction tx = ses.beginTransaction();
+                ses.delete(member);
+                tx.commit();
+                ses.close();
+
+
+        if (source == addMember) {
+            new DataSaver();
+        } else if (source == remMember) {
+            Members member = table.getModel().getList(table.getSelectedRow());
+
+            Session ses = HibernateUtil.openSession();
+                org.hibernate.Transaction tx = ses.beginTransaction();
+                ses.delete(member);
+                tx.commit();
+                ses.close();
+
+            } else if (source == cancel) {
+                clearInfo();
+
+
+            }
         }
-        else if (source == remMember){
-            System.out.println("Usuwa członka");
-        }
-        else if(source == back){
-//            this.dispose();
+
         }
 
 
+    }
+
+
+    private class DataSaver extends JPanel implements ActionListener {
+        private Persons person;
+        private Members member;
+        private JFrame frame;
+
+        JLabel name = new JLabel("Name:");
+        JLabel surname = new JLabel("Surname:");
+        JLabel email = new JLabel("E-mail:");
+        JLabel mobile = new JLabel("Mobile:");
+
+        JTextArea nameTxt = new JTextArea();
+        JTextArea surnameTxt = new JTextArea();
+        JTextArea emailTxt = new JTextArea();
+        JTextArea mobileTxt = new JTextArea();
+
+        JButton save = new JButton("Save");
+        JButton cancel = new JButton("Cancel");
+
+
+
+
+        public DataSaver() {
+
+            setLayout(new GridLayout(5, 2));
+
+            add(name);
+            add(nameTxt);
+            add(surname);
+            add(surnameTxt);
+            add(email);
+            add(emailTxt);
+            add(mobile);
+            add(mobileTxt);
+
+            add(save);
+            save.addActionListener(this);
+            add(cancel);
+            cancel.addActionListener(this);
+
+            frame = new JFrame("Set data");
+            frame.add(this);
+            frame.setSize(500, 500);
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            frame.setVisible(true);
+
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Object source = e.getSource();
+
+            if (source == save) {
+
+                person = new Persons();
+                person.setName(nameTxt.getText());
+                person.setSurname(surnameTxt.getText());
+                person.setEmail(emailTxt.getText());
+                person.setMobile(mobileTxt.getText());
+
+                member = new Members();
+                member.setPerson(person);
+
+                /*Session ses = HibernateUtil.openSession();
+                org.hibernate.Transaction tx = ses.beginTransaction();
+
+                ses.persist(person);
+                ses.persist(member);
+                tx.commit();
+                ses.close();*/
+                clearInfo();
+
+                int i =  JOptionPane.showConfirmDialog(null, "Do You want add next Member?",
+                        null, JOptionPane.YES_NO_OPTION);
+
+                if(i==1){
+                    frame.dispose();
+                }
+
+            } else if (source == cancel) {
+                clearInfo();
+
+
+            }
+        }
+
+        private void clearInfo(){
+            nameTxt.setText("");
+            surnameTxt.setText("");
+            emailTxt.setText("");
+            mobileTxt.setText("");
+        }
     }
 
 }
